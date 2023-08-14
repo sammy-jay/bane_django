cae23678e5ffe648797175dd817b450aedd6797e
sammy-jay <sammyjayden27@gmail.com>
2023-08-14 03:25:35 +0100

    added auth urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from . import views
+from . import views, auth
 
 urlpatterns = [
+    # auth urls
+    path('auth/register', auth.UserRegistrationView.as_view()),
+    path('auth/login', auth.UserLoginView.as_view()),
+    path('auth/logout', auth.UserLogoutView.as_view()),
+
+    # posts urls
     path('posts/', views.PostView.as_view()),
     path('posts/<int:id>', views.PostDetailView.as_view()),
 ]
