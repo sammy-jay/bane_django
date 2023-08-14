0dafca898a2e7ab61b798a231cdea0d7394f45e9
sammy-jay <sammyjayden27@gmail.com>
2023-08-14 03:26:02 +0100

    playing around with authorizations

--- api/views.py
+++ api/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import get_list_or_404, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import authentication_classes, permission_classes
 
-from .serializers import UserSerializer, PostSerializer
-from .models import User, Post
+from .serializers import PostSerializer
+from .models import Post
 
 class PostView(APIView):
+    # authentication_classes = [TokenAuthentication]
+    
+    @authentication_classes([TokenAuthentication])
+    @permission_classes([IsAuthenticated])
     def get(self, request):
+        print(request.user)
         posts = get_list_or_404(Post)
         serializer = PostSerializer(posts, many=True)
         return Response({"data": serializer.data})
 
+    @permission_classes([IsAuthenticated])
     def post(self, request):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
@@ -20,11 +30,14 @@ class PostView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PostDetailView(APIView):
+    # permission_classes = [IsAuthenticated]
+
     def get(self, request, id):
         post = get_object_or_404(Post, id=id)
         serializer = PostSerializer(post)
         return Response({"data": serializer.data})
 
+    @permission_classes([IsAuthenticated])
     def patch(self, request, id):
         post = get_object_or_404(Post, pk=id)
         serializer = PostSerializer(post, data=request.data)
@@ -33,9 +46,10 @@ class PostDetailView(APIView):
             return Response({"data": serializer.data})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @permission_classes([IsAuthenticated])
     def delete(self, request, id):
         post = get_object_or_404(Post, pk=id)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
-   
+# context={'request': request}
